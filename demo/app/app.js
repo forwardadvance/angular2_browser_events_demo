bc88d7367413b2c11fbfdf7ad20e69397f43767a
Nicholas Johnson <nicholas@forwardadvance.com>
2016-11-15 14:44:37 +0000

    Answers

--- demo/app/app.js
+++ demo/app/app.js
@@ -8,27 +8,26 @@ var AppComponent = ng.core
     selector: "app",
     template:
     `
-      <a on-click="handleClick($event)">Click</a>
+      <a on-click="handleClick('hello')">Click</a>
       <br />
-      <input on-input="value = $event.target.value" />
-      {{value}}
-
-      <form on-submit="submitted = true">
-        <input />
-        <button>submit</button>
-        {{submitted}}
-      </form>
+      <a (click)="liked = liked + 1">Like me!</a> {{liked}}
+      <br />
+      <input (input)="handleInput($event)" /> {{value}}
+      <br />
+      <input (input)="value2 = $event.target.value" /> {{value2}}
     `
   })
   .Class({
     constructor: function() {
+      this.liked = 0;
+      this.value = "";
       this.handleClick = function(evt) {
-        console.log('clicked');
-      }
+        console.log('clicked', evt);
+      };
       this.handleInput = function(evt) {
         console.log(evt);
         this.value = evt.target.value;
-      }
+      };
     }
   })
 
@@ -54,4 +53,13 @@ ng.platformBrowserDynamic
   .platformBrowserDynamic()
   .bootstrapModule(AppModule);
 
+    // <a on-click="handleClick($event)">Click</a>
+    // <br />
+    // <input on-input="value = $event.target.value" />
+    // {{value}}
 
+    // <form on-submit="submitted = true">
+    //   <input />
+    //   <button>submit</button>
+    //   {{submitted}}
+    // </form>
---
+++ exercise_escape_the_dungeon_answers/app/app.js
@@ -0,0 +1,77 @@
+// Exercise - Mouse Coordinates
+
+// We are going to access the mouse coordinates in a div
+// When the user clicks on the div, grab the mouse coordinates from the event and output them on the page.
+// For bonus points, make this happen when on mousemove
+
+var heroModel = {
+  name: "LLars Bunderchump",
+  x: 0,
+  y: 0,
+  moveNorth: function() {
+    this.x++;
+  },
+  moveSouth: function() {
+    this.x--;
+  },
+  moveEast: function() {
+    this.y--
+  },
+  moveWest: function() {
+    this.y++
+  }
+}
+
+var AppComponent = ng.core
+  .Component({
+    selector: 'app',
+    template:
+    `
+      <ul>
+        <li>
+          <a (click)="protagonist.moveEast()">East</a>
+        </li>
+        <li>
+          <a (click)="protagonist.moveWest()">West</a>
+        </li>
+        <li>
+          <a (click)="protagonist.moveNorth()">North</a>
+        </li>
+        <li>
+          <a (click)="protagonist.moveSouth()">South</a>
+        </li>
+      </ul>
+
+      <h2>{{protagonist.name}}</h2>
+      x: {{protagonist.x}}, y: {{protagonist.y}}
+    `
+  })
+  .Class({
+    constructor: function() {
+      this.protagonist = heroModel;
+    }
+  });
+
+
+/**
+  * The App Module
+  */
+var AppModule =
+  ng.core.NgModule({
+    imports: [ ng.platformBrowser.BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent ]
+  })
+  .Class({
+    constructor: function() {}
+  });
+
+
+/**
+  * Finally bootstrap
+  */
+ng.platformBrowserDynamic
+  .platformBrowserDynamic()
+  .bootstrapModule(AppModule);
+
+
